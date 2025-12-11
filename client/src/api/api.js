b7fcf73886313969fd2928e630b80892ddb0c86b
unknown <muralikrishnag2001@gmail.com>
2025-12-11 23:59:55 +0530

    Final Version 01.00.03.00

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://electronicsweb.onrender.com/api",
+  baseURL: "http://localhost:5000/api", // your backend URL
 });
 
 // attach token automatically
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -38,7 +38,7 @@ export default function Signup() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("http://electronicsweb.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
